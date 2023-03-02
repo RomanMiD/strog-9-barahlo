ec874ad3f2cbf5e172eb106107e2f7cd70804979
RomanMiD <romanmikhailov71@gmail.com>
2023-03-02 14:53:43 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  form : any
+  
+  constructor(private fb: FormBuilder){
+  }
+  initForm(){
+    this.form = this.fb.group({
+      input : '',
+      output: ''
+    })
+  }
+  ngOnInit(){
+    this.initForm()
+  }
+  onClick(){
+    let regex = '📦.*'
+    let matches = [...(this.form.value.input.matchAll(regex))]
+    let result = Array.from(matches, m=> m[0])
+    result.map((val, i) => result[i] = val.replace(")", "").replace(" (", " | "))
+    console.log(result)
+    this.form.patchValue({output: result.join("\n")})
+  }
+  
 }
